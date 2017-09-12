35cd30a974eeb0d5c5e310a28c5076c6b325a588
Guillermo Herrero <guillermo.herrero.i@gmail.com>
2017-09-12 15:05:16 +0200

    Update files

--- docker-nexus.sh
+++ docker-nexus.sh
@@ -7,8 +7,11 @@ echo "$HEADER in process..."
 CADDY=$HOME/caddy
 mkdir -p $CADDY
 
-echo "nexus.beyond-coding.com" >> $CADDY/Caddyfile
-echo "tls guillermo.herrero@beyond-coding.com" >> $CADDY/Caddyfile
+wget https://raw.githubusercontent.com/beyond-coding/docker-nexus/master/Caddyfile
+mv Caddyfile $CADDY/.
+
+# echo "nexus.beyond-coding.com" >> $CADDY/Caddyfile
+# echo "tls guillermo.herrero@beyond-coding.com" >> $CADDY/Caddyfile
 
 mkdir nexus && chown -R 200 nexus
 
