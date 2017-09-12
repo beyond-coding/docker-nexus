a5cdd53731aa30d8de4fc161cb6a7df2a229146f
Guillermo Herrero <guillermo.herrero.i@gmail.com>
2017-09-12 13:11:39 +0200

    Update files

--- docker-nexus.sh
+++ docker-nexus.sh
@@ -10,8 +10,10 @@ mkdir -p $CADDY
 echo "nexus.beyond-coding.com" >> $CADDY/Caddyfile
 echo "tls guillermo.herrero@beyond-coding.com" >> $CADDY/Caddyfile
 
+mkdir nexus
+
 echo "$HEADER starting..."
 
 wget https://raw.githubusercontent.com/beyond-coding/docker-nexus/master/docker-compose.yml
 
-docker-compose up
+docker-compose -d up
