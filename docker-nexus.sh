85dc82594b988b0cc9c723f73501f44dd3e903d0
Guillermo Herrero <guillermo.herrero.i@gmail.com>
2017-09-13 10:36:59 +0200

    Update files

--- docker-nexus.sh
+++ docker-nexus.sh
@@ -4,22 +4,24 @@ HEADER="Docker nexus -"
 
 echo "$HEADER in process..."
 
-CADDY=$HOME/caddy
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+CADDY=$DIR/caddy
 mkdir -p $CADDY
 
 wget https://raw.githubusercontent.com/beyond-coding/docker-nexus/master/Caddyfile
 cp Caddyfile $CADDY/.
 rm Caddyfile
 
-mkdir $CADDY/srv
-wget https://raw.githubusercontent.com/beyond-coding/docker-nexus/master/index.html
-cp index.html $CADDY/srv/.
-rm index.html
+# mkdir $CADDY/srv
+# wget https://raw.githubusercontent.com/beyond-coding/docker-nexus/master/index.html
+# cp index.html $CADDY/srv/.
+# rm index.html
 
 # echo "nexus.beyond-coding.com" >> $CADDY/Caddyfile
 # echo "tls guillermo.herrero@beyond-coding.com" >> $CADDY/Caddyfile
 
-mkdir nexus
+# mkdir nexus
 
 echo "$HEADER starting..."
 
